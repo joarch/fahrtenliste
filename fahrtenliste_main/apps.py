b32c196acd93b83fd68563cb9f12e1b9ea669b37
jorgortmann <dev.mac.pycharm@ortmann.pt>
2020-05-19 23:38:23 +0200

    Programmversion ausgeben

--- fahrtenliste_main/apps.py
+++ fahrtenliste_main/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
 
+import version
+
 
 class FahrtenlisteConfig(AppConfig):
     name = 'fahrtenliste_main'
     verbose_name = ' Fahrtenliste'
+
+
+print(f"Version: {version.VERSION}")
